d86831fe45d2c511329ce1df6f305efe4d8da006
Furkan İlbahar <furkan@pirireis.com.tr>
2020-06-29 16:36:02 +0300

    New answers

--- src/deneme/leetcode/easy/EasyQuestions.java
+++ src/deneme/leetcode/easy/EasyQuestions.java
@@ -233,4 +233,37 @@ public class EasyQuestions {
         return Math.max(resultArray[nums.length - 2], resultArray[nums.length - 1]);
     }
 
+
+    /* 231. Power of Two
+    Given an integer, write a function to determine if it is a power of two.
+        Example 1:
+            Input: 1
+            Output: true
+            Explanation: 20 = 1
+        Example 2:
+            Input: 16
+            Output: true
+            Explanation: 24 = 16
+        Example 3:
+            Input: 218
+            Output: false*/
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        int bolum = n;
+        int kalan = 0;
+        while (bolum > 1) {
+            kalan = bolum % 2;
+            if (kalan != 0) {
+                return false;
+            }
+            bolum = bolum / 2;
+        }
+        return true;
+    }
+
 }
--- src/deneme/leetcode/medium/MediumQuestions.java
+++ src/deneme/leetcode/medium/MediumQuestions.java
@@ -1,6 +1,7 @@
 package deneme.leetcode.medium;
 
 import deneme.leetcode.ListNode;
+import deneme.leetcode.easy.EasyQuestions;
 
 import java.util.Arrays;
 
@@ -78,6 +79,39 @@ public class MediumQuestions {
             }
         }
         return nums[0];
+    }
+
+
+    /* 213. House Robber II
+    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed.
+    All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
+    Meanwhile, adjacent houses have security system connected and it will automatically contact the police
+    if two adjacent houses were broken into on the same night. Given a list of non-negative integers representing the
+    amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+        Example 1:
+            Input: [2,3,2]
+            Output: 3
+            Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+        Example 2:
+            Input: [1,2,3,1]
+            Output: 4
+            Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3). Total amount you can rob = 1 + 3 = 4.*/
+    public static int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] nums1 = new int[nums.length - 1];
+        int[] nums2 = new int[nums.length - 1];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            nums1[i] = nums[i];
+            nums2[i] = nums[i + 1];
+        }
+        return Math.max(EasyQuestions.rob(nums1), EasyQuestions.rob(nums2));
     }
 
 
