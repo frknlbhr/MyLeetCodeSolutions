b1981c9d2282f3131d3707daff6b29259c926b66
Furkan İlbahar <furkan@pirireis.com.tr>
2020-08-21 11:00:10 +0300

    151 - Reverse Words in a String

--- src/deneme/leetcode/medium/MediumQuestions.java
+++ src/deneme/leetcode/medium/MediumQuestions.java
@@ -82,6 +82,33 @@ public class MediumQuestions {
     }
 
 
+    /* 151. Reverse Words in a String
+    Given an input string, reverse the string word by word.
+        Example 1:
+            Input: "the sky is blue"
+            Output: "blue is sky the"
+        Example 2:
+            Input: "  hello world!  "
+            Output: "world! hello"
+            Explanation: Your reversed string should not contain leading or trailing spaces.
+        Example 3:
+            Input: "a good   example"
+            Output: "example good a"
+            Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+    Note: A word is defined as a sequence of non-space characters.
+    Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+    You need to reduce multiple spaces between two words to a single space in the reversed string.*/
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(" ");
+            result.append(words[i]);
+        }
+        return result.substring(1);
+    }
+
+
     /* 213. House Robber II
     You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed.
     All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
